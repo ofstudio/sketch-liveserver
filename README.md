45a3b4dd2d8ca8799e62b0ca18131126fdfacffc
Oleg Fomin <ofstudio@gmail.com>
2018-07-21 21:59:36 +0300

    Inital commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# sketch-liveserver
+
+Serves artboard previews from [Skala Preview Sketch Plugin](https://bjango.com/help/skalapreview/sketch/) across network via http. Live reload supported via BrowserSync.
+
+
+
+# Installation
+
+1. Install [NodeJS](https://nodejs.org/en/download/) 8 or higher
+2. `cd sketch-liveserver`
+3. Run `npm install`
+4. Install [Scala Preview](https://bjango.com/mac/skalapreview/) and [Skala Preview Sketch Plugin](https://bjango.com/help/skalapreview/sketch/)
+
+# Run
+1. Run `./sketch-liveserver`
+2. Create new Sketch document and at least 1 artboard within it (or open existing document)
+3. Enable Auto Preview: `Plugins > Sketch Preview > Toggle Auto Preview`
+4. Open [http://localhost:3000](http://localhost:3000) in your browser
---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const browserSync = require('browser-sync')
+const connectBrowserSync = require('connect-browser-sync')
+const fsevents = require('fsevents')
+const http = require('http')
+const path = require('path')
+
+const watchDir = process.env.WATCH_DIR
+if (typeof watchDir !== 'string' || watchDir.length < 1) {
+  console.log ('`WATCH_DIR` environment variable not specified!\nExiting.')
+  process.exit(0)
+}
+
+const app = express()
+const bs = browserSync.create().init({logSnippet: false})
+let imageFileName = path.join(__dirname, 'default.png')
+
+app.set('views', __dirname)
+app.set('view engine', 'hbs')
+app.use(connectBrowserSync(bs))
+app.get('/', (req, res) => res.render('index'))
+app.get('/image', (req, res) => res.sendFile(imageFileName))
+
+const port = 3000
+http.createServer(app).listen(port, function () {
+  console.log('Listening on port ' + port + '...')
+})
+
+const watcher = fsevents(watchDir)
+watcher.on('change', (path, info) => {
+  if (info && info.event === 'moved-in') {
+    imageFileName = path
+    setTimeout(() => bs.publicInstance.reload('/image'), 1000)
+  }
+})
+watcher.start()
+console.log(`Watching for changes in ${watchDir}`)
+
+const shutDown = (msg, code) => {
+  return () => {
+    watcher.stop()
+    console.log(`\n${msg}\nExiting app (${code})\n`)
+    process.exit(code)
+  }
+}
+process.on('SIGTERM', shutDown('Terminated (SIGTERM)', 2));
+process.on('SIGINT', shutDown('Interrupted (SIGINT)', 15));
